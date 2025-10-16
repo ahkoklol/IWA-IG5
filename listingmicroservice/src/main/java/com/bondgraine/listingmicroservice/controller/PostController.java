3aa7e934971c4021f6b07daeb49812c8b4769729
wayne <wayne.wan-chow-wah@etu.umontpellier.fr>
2025-10-16 13:53:41 +0400

    Add post implementation

--- listingmicroservice/src/main/java/com/bondgraine/listingmicroservice/controller/PostController.java
+++ listingmicroservice/src/main/java/com/bondgraine/listingmicroservice/controller/PostController.java
@@ -4,11 +4,9 @@ import com.bondgraine.listingmicroservice.entity.Post;
 import com.bondgraine.listingmicroservice.service.PostService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/post")
@@ -20,9 +18,41 @@ public class PostController {
 
     @GetMapping("/{postId}")
     public ResponseEntity<Post> getPost(@PathVariable("postId") String postId) {
+        log.info("Request to get post with id {}", postId);
         return postService.getPostById(postId)
                 .map(ResponseEntity::ok) // 200 OK with body
                 .orElseGet(() -> ResponseEntity.notFound().build()); // 404 Not Found
+    }
+
+    @PostMapping("/{postId}")
+    public ResponseEntity<Post> updatePost(@RequestBody Post post, @PathVariable String postId) {
+        log.info("Received request to edit post: {}", post);
+        // The JSON payload is automatically mapped to the 'Post' object
+        Post updatedPost = postService.updatePost(postId, post);
+
+        if (updatedPost != null) {
+            log.info("Updated post with id {}", postId);
+            return ResponseEntity.ok(updatedPost); // 200 ok with updated entity
+        } else {
+            return ResponseEntity.notFound().build(); // 404 not found id
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Post> createPost(@RequestBody Post post) {
+        log.info("Received request to create post: {}", post);
+        Post createdPost = postService.createPost(post);
+        return new ResponseEntity<>(createdPost, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/{postId}/hide")
+    public ResponseEntity<Post> hidePost(@PathVariable String postId) {
+        log.info("Received request to hide post: {}", postId);
+        if(postService.hidePost(postId)) {
+            return ResponseEntity.ok().build(); // 200 ok if post was updated
+        } else {
+            return ResponseEntity.notFound().build(); // 404 not found id
+        }
     }
 
 }
--- listingmicroservice/src/main/java/com/bondgraine/listingmicroservice/service/PostService.java
+++ listingmicroservice/src/main/java/com/bondgraine/listingmicroservice/service/PostService.java
@@ -6,7 +6,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class PostService {
@@ -15,7 +17,56 @@ public class PostService {
 
     private PostRepository postRepository;
 
+    /**
+     * Fetch a post by id
+     * @param postId the id of the post
+     * @return a Post object
+     */
     public Optional<Post> getPostById(String postId) {
         return postRepository.findById(postId);
+    }
+
+    /**
+     * Update a post by id
+     * @param postId the id of the post
+     * @param post the Post object with updated info
+     * @return the updated Post object
+     */
+    public Post updatePost(String postId, Post post) {
+
+        // Throw NoSuchElementException if post with id postId not found
+        getPostById(postId).orElseThrow();
+        return postRepository.save(post);
+    }
+
+    /**
+     * Create a post
+     * @param post the Post object to create
+     */
+    public Post createPost(Post post) {
+        post.setPost_id(UUID.randomUUID().toString());
+        return postRepository.save(post);
+    }
+
+    /**
+     * Hide a post - change status to "hidden"
+     * @param postId the id of the post
+     * @return true if the post was successfully hidden, false otherwise
+     */
+    public boolean hidePost(String postId) {
+        Optional<Post> optionalPost = getPostById(postId);
+        return optionalPost.map(post -> {
+            // If the post is present (inside the Optional):
+
+            // 3. Apply the required changes (business logic)
+            post.setStatus("hidden");
+            post.setDate_modified(new Date()); // Update modification timestamp
+
+            // 4. Save the modified entity. Hibernate performs an UPDATE.
+            postRepository.save(post);
+
+            // 5. Return true inside the map function, indicating the operation succeeded.
+            return true;
+        }).orElse(false);
     }
 }
