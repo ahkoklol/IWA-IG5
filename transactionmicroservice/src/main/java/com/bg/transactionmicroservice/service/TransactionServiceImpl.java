0b2c4395483339b7c7ea6f9cc9e7e7add7046ea5
wayne <wayne.wan-chow-wah@etu.umontpellier.fr>
2025-10-22 18:09:41 +0400

    Fix grpc config

--- transactionmicroservice/src/main/java/com/bg/transactionmicroservice/service/TransactionServiceImpl.java
+++ transactionmicroservice/src/main/java/com/bg/transactionmicroservice/service/TransactionServiceImpl.java
@@ -3,12 +3,16 @@ package com.bg.transactionmicroservice.service;
 import com.bg.transactionmicroservice.entity.Transaction;
 import com.bg.transactionmicroservice.grpc.PurchaseRequest;
 import com.bg.transactionmicroservice.grpc.PurchaseResponse;
+import com.bg.transactionmicroservice.grpc.TransactionServiceGrpc;
+import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
+import io.grpc.stub.StreamObserver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.grpc.server.service.GrpcService;
 
 @GrpcService
-public class TransactionServiceImpl {
+public class TransactionServiceImpl extends TransactionServiceGrpc.TransactionServiceImplBase {
 
     private static final Logger log = LoggerFactory.getLogger(TransactionServiceImpl.class);
 
@@ -18,7 +22,8 @@ public class TransactionServiceImpl {
         this.transactionService = transactionService;
     }
 
-    public PurchaseResponse purchase(PurchaseRequest purchaseRequest) {
+    @Override
+    public void purchase(PurchaseRequest purchaseRequest, StreamObserver<PurchaseResponse> responseObserver) {
         Transaction transaction = new Transaction();
         transaction.setStatus("ok");
         transaction.setCommission(2);
@@ -28,16 +33,33 @@ public class TransactionServiceImpl {
 
         Transaction purchaseResult = transactionService.purchase(transaction);
 
-        if (purchaseResult == null) {
-            return PurchaseResponse.newBuilder()
-                    .setSuccess(false)
-                    .setErrorMessage("problem")
-                    .build();
+        PurchaseResponse response;
+        try {
+            if (purchaseResult == null) {
+                log.info("Problem while trying to purchase {}", purchaseRequest.getPostId());
+                response = PurchaseResponse.newBuilder()
+                        .setSuccess(false)
+                        .setErrorMessage("problem")
+                        .build();
+            } else {
+                log.info("Purchase successfull");
+                response = PurchaseResponse.newBuilder()
+                        .setSuccess(true)
+                        .build();
+            }
+            responseObserver.onNext(response);
+            responseObserver.onCompleted();
+        } catch (Exception e) {
+            log.error("Error processing purchase", e);
+            responseObserver.onError(e);
         }
+    }
 
-        return PurchaseResponse.newBuilder()
-                .setSuccess(true)
-                .build();
+    @Override
+    public PurchaseResponse purchase(PurchaseRequest buyPostRequest) {
+        throw new StatusRuntimeException(Status.UNIMPLEMENTED.withDescription(
+                "Synchronous/Blocking method BuyPost is not implemented. Use the AsyncService contract."
+        ));
     }
 
 }
