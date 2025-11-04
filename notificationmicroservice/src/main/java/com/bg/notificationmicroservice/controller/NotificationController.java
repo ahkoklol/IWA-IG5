05fc02a1ea6405a9cf76da0c30568d888e5e0719
wayne <wayne.wan-chow-wah@etu.umontpellier.fr>
2025-11-04 13:27:17 +0400

    Add notification microservice implementation

---
+++ notificationmicroservice/src/main/java/com/bg/notificationmicroservice/controller/NotificationController.java
@@ -0,0 +1,51 @@
+package com.bg.notificationmicroservice.controller;
+
+import com.bg.notificationmicroservice.entity.Notification;
+import com.bg.notificationmicroservice.service.NotificationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/notification")
+public class NotificationController {
+
+    private static final Logger log = LoggerFactory.getLogger(NotificationController.class);
+
+    private final NotificationService notificationService;
+
+    public NotificationController(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Notification> createNotification(@RequestBody Notification notification) {
+        Notification createdNotification = notificationService.createNotification(notification);
+        if (createdNotification == null) {
+            log.warn("Notification could not be created");
+            return ResponseEntity.badRequest().build();
+        }
+        log.info("Notification created");
+        return ResponseEntity.ok(createdNotification);
+    }
+
+    @GetMapping("/{clientId}")
+    public ResponseEntity<List<Notification>> getNotifications(@PathVariable String clientId) {
+        List<Notification> notificationList = notificationService.getAllNotifications(clientId);
+        if (notificationList.isEmpty()) {
+            log.info("No notifications for client {}", clientId);
+        }
+        log.info("Found {} notifications", notificationList.size());
+        return ResponseEntity.ok(notificationList);
+    }
+
+    @PutMapping("/{clientId}")
+    public ResponseEntity<Void> markNotificationAsRead(@PathVariable String clientId) {
+        notificationService.markNotificationAsRead(clientId);
+        return ResponseEntity.ok().build();
+    }
+
+}
