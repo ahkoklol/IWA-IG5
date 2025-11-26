ba58b0d5b833ec7ce26c14609457c01352cce3dc
DAVID Seb <daviseb23@gmail.com>
2025-11-26 10:27:10 +0100

    Add reporting service to eureka

--- reportingmicroservice/src/main/java/com/bg/reportingmicroservice/ReportingmicroserviceApplication.java
+++ reportingmicroservice/src/main/java/com/bg/reportingmicroservice/ReportingmicroserviceApplication.java
@@ -2,8 +2,10 @@ package com.bg.reportingmicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ReportingmicroserviceApplication {
 
 	public static void main(String[] args) {
